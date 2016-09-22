39e735d95e1ace15b8f23fe2f8ef9b53249825f4
Yulia Yaschun <jukiyaschun@gmail.com>
2016-09-22 10:59:29 +0300

    do acordion

--- js/main.js
+++ js/main.js
@@ -2,43 +2,69 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
  var box = document.getElementsByClassName('box')[0];
- console.log(box);
  var start = document.getElementsByClassName('start')[0];
  var stop = document.getElementsByClassName('stop')[0];
  var reset = document.getElementsByClassName('reset')[0]; 
- var myWidth = 1000;
+ var accordion = document.getElementById('accordion');
+ var items = accordion.getElementsByTagName('p'); 
+ var lists = accordion.getElementsByClassName('list-container'); 
+ 
  var firstWidth = getComputedStyle(box).width;
- var timer;
 
- start.addEventListener('click', function() {
+ go(1500);
 
-   clearInterval(timer);
-   
-    startWidth = parseInt(getComputedStyle(box).width);
-    console.log(myWidth);
+ function go(myWidth) {
+   var myWidth = myWidth || 1000;
+   var timer; 
+   console.log(myWidth);
+   start.addEventListener('click', function(e) {
 
-    timer = setInterval(function() {
-        startWidth += 10;
-        
-        box.style.width = startWidth + 'px';
+	 clearInterval(timer);
+	   
+	 startWidth = parseInt(getComputedStyle(box).width);
 
-        if ( parseInt(getComputedStyle(box).width) > myWidth) clearInterval(timer);
+	 timer = setInterval(function() {
+	   startWidth += 10;
+			
+	   box.style.width = startWidth + 'px';
 
-      }, 30);
+	   if ( parseInt(getComputedStyle(box).width) > myWidth) clearInterval(timer);
 
-  });
+	 }, 30);
 
-  stop.addEventListener('click', function() {
-   
-    clearInterval(timer);
+   });
 
-  });
+   stop.addEventListener('click', function() {
+	   
+	 clearInterval(timer);
 
-  reset.addEventListener('click', function() {
-   
-    box.style.width = firstWidth;
+   });
 
-  });
+   reset.addEventListener('click', function() {
+
+	 clearInterval(timer);
+	 box.style.width = firstWidth;
+
+   });
+ }
+ 
+ accordion.addEventListener('click', function (event) {
+	for (var i = 0; i < items.length; i++) {
+		lists[i].style.overflow = 'hidden';
+		lists[i].style.height = '0';
+		if (event.target == items[i]){
+			if (getComputedStyle(lists[i]).overflow == 'hidden'){
+				lists[i].style.overflow = 'auto';
+				lists[i].style.height = '130px';				
+			} else {
+				lists[i].style.overflow = 'hidden';
+				lists[i].style.height = '0';				
+			}		 
+		}
+	}
+	 
+ });
+ 
 }
 
 
